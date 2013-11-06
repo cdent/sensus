6ec23a28a092c9a1a5c98d5b545f1bf4fb7cf61e
Chris Dent <chris.dent@gmail.com>
2013-11-06 12:40:49 +0000

    Turn into a tsapp, update for modern jquery

--- assets/sensus.js
+++ assets/sensus.js
@@ -144,8 +144,8 @@ $('.gohome').click(goHome);
 $('.goleft').click(goRight);
 $('.goright').click(goLeft);
 $('.goend').click(goEnd);
-$('.left').live('click', goRight);
-$('.right').live('click', goLeft);
+$('.left').on('click', goRight);
+$('.right').on('click', goLeft);
 
 // keyup doesn't work here: screen scrolls
 // probably should use switch
